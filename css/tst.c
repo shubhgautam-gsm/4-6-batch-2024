48791b1a843c6985e6c6edd981052ea2b2ffa61f
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-01-17 11:24:08 +0530

    17-01-25

---
+++ css/tst.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	divyang();
+    danish();
+    return 0;
+}
+
+
+int divyang(){
+	printf("divyang hajir chu World!\n");
+};
+
+int danish(){
+	printf("danish hajir chu World!");
+}
