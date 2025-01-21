f6f5696d22371b0a3a4220c1b3d1fd1805de1599
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-01-21 17:52:10 +0530

    21-01-25

---
+++ js/js/pro8.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    let x = document.getElementsByClassName("demo")
+
+    let start = document.getElementById("vol").value
+    
+    let end = document.getElementById("vol2").value
+    
+   // let user_choose =document.getElementById("vol3").value
+  //  let user_choose =prompt('write color name:')
+
+    for (let i = start; i <= end ; i++) {
+        let user_choose =prompt('write color name:') // repeat asking choice
+        // DYANMIC CODE
+        x[i].style.color = user_choose;
+        //      x[i].style.color = "rgb(255,255,255)
+    }
+}
