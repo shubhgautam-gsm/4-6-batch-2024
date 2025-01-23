9c4f0c85aeeadd4e48791f7cd6758c2b8edc835e
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-01-23 17:57:02 +0530

    23-01-25

--- js/js/pro4.js
+++ js/js/pro4.js
@@ -2,4 +2,5 @@
     function chg(){
         document.getElementById('chg').style.color='red';
         document.getElementById('chg').style.fontFamily='cursive'
+        document.getElementById('chg').innerHTML='hello world';
     }
