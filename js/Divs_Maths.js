16ccf359f3ac809d5e30fda5fd2434cc8955b6c7
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-01-30 18:00:50 +0530

    30-1-24

---
+++ js/Divs_Maths.js
@@ -0,0 +1,15 @@
+
+function JODO(a,b,c){
+    return a+b+c;
+}
+function takavari(a,b,c){
+    return (a+b+c)/3;
+}
+function pow(x,a){
+    return x**a;
+}
+
+function gamti(x,a){
+    n=a % x
+    return n;
+}
