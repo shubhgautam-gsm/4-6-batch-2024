0463f1dc3a5f5a3214169e4c9002a5e0178c71a1
shubhgautam-gsm <143082586+shubhgautam-gsm@users.noreply.github.com>
2025-02-04 11:17:48 +0530

    4-2-25

---
+++ js/js/add.js
@@ -0,0 +1,14 @@
+function add(){
+
+    let category = document.getElementById("category").value;
+    let proname= document.getElementById("proname").value;
+    let price = document.getElementById('price').value;
+    
+    
+
+    document.getElementById('add_product').innerHTML = category+proname+price
+
+
+
+    
+}
